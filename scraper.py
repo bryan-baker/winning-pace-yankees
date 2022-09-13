d2c4a0571859aedc57758a354b6b85f19685d18c
Bryan Baker <107493329+bryan-baker@users.noreply.github.com>
2022-09-13 17:39:43 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -28,31 +28,21 @@ from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-
-options = Options()
-# browser is Chromium instead of Chrome
-options.BinaryLocation = "/usr/bin/chromium-browser"
-# we use custom chromedriver for raspberry
-driver_path = "/usr/bin/chromedriver"
-driver = webdriver.Chrome(options=options, service=Service(driver_path))
 
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
-# chrome_options = Options()
-# options = [
-   # "--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage"
-# ]
-# for option in options:
-   # chrome_options.add_argument(option)
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+   chrome_options.add_argument(option)
 
 # In[2]:
 
